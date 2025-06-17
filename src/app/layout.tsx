ef292832af5e3c05bd1bed5789c05f6e37a15113
aniket-patil7075 <akp7075@gmail.com>
2025-06-17 17:18:07 +0530

    Booking request all columns added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { SubcategoryProvider } from './context/SubCategoryContext';
 import { ServiceProvider } from './context/ServiceContext';
 import { SubscribeProvider } from './context/SubscribeContext';
 import { ProviderContextProvider } from '@/context/ProviderContext';
+import { CheckoutProvider } from './context/CheckoutContext';
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -32,7 +33,9 @@ export default function RootLayout({
                     <ServiceProvider>
                       <SubscribeProvider>
                         <ProviderContextProvider>
-                          {children}
+                          <CheckoutProvider>
+                            {children}
+                          </CheckoutProvider>
                         </ProviderContextProvider>
                       </SubscribeProvider>
                     </ServiceProvider>
