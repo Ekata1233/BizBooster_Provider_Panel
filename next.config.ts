09c921a28ae881d0d2a87edfc0e4c37447b20f9b
aniket-patil7075 <akp7075@gmail.com>
2026-01-08 17:50:36 +0530

    solve vercel error

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,9 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   images: {
-//     domains: ["ik.imagekit.io"], // ✅ Allow external images from ImageKit
-//   },
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ["@svgr/webpack"],
-//     });
-//     return config;
-//   },
-//   turbopack: {},
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        pathname: '/**',
-      },
-      // Add more patterns if needed:
-      // {
-      //   protocol: 'https',
-      //   hostname: 'example.com',
-      //   pathname: '/**',
-      // },
-    ],
+    domains: ["ik.imagekit.io"], // ✅ Allow external images from ImageKit
   },
   webpack(config) {
     config.module.rules.push({
